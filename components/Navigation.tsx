5d8a68992445ad1b2678311974d8da913e459ec0
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2022-11-12 08:49:11 +0630

    temporarily dont use animation for drawer cuz of css bug

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -126,11 +126,30 @@ const moderatorAcademyData = [
   },
 ];
 
+const ButtonGroup = () => {
+  return (
+    <div className="absolute bottom-6 z-10 flex justify-between items-center w-fit">
+      <button className="px-5 py-2 text-white text-sm bg-[#5865f2] rounded-full flex items-center">
+        <img src="/download.png" className=" w-4 h-4 mr-3" />
+        Download for Linux
+      </button>
+      <button className="border-solid border border-[#5865f2] rounded-full ml-2 px-7 py-2 text-sm normal-font text-[#5865f2] block sm:hidden">
+        Login
+      </button>
+    </div>
+  );
+};
+
 export const Navigation = () => {
   const router = useRouter();
   const [isSlideOpen, setIsSlideOpen] = useState(false);
   const [isSubSlideOpen, setIsSubSlideOpen] = useState(false);
   const [currentSubNav, setCurrentSubNav] = useState("Safety");
+
+  useEffect(() => {
+    console.log("Slide " + isSlideOpen);
+    console.log("SubSlide " + isSubSlideOpen);
+  });
 
   const closeAll = () => {
     setIsSlideOpen((prev) => {
@@ -179,10 +198,8 @@ export const Navigation = () => {
         />
       </div>
       <div
-        className={`${
-          NavStyles.sideNav
-        } h-screen w-full md:w-4/12  bg-white rounded-l-md absolute z-10 right-0 ${
-          isSlideOpen ? NavStyles.sideNavOpen : NavStyles.sideNavClose
+        className={`h-full w-full md:w-4/12  bg-white rounded-l-md absolute z-10 right-0 ${
+          isSlideOpen ? "block" : "hidden"
         } top-0`}
       >
         <div className="flex justify-between mx-6">
@@ -229,15 +246,7 @@ export const Navigation = () => {
                   </li>
                 ))}
             </ul>
-            <div className="absolute bottom-6 z-10 flex justify-between items-center w-fit">
-              <button className="px-5 py-2 text-white text-sm bg-[#5865f2] rounded-full flex items-center">
-                <img src="/download.png" className=" w-4 h-4 mr-3" />
-                Download for Linux
-              </button>
-              <button className="border-solid border border-[#5865f2] rounded-full ml-2 px-7 py-2 text-sm normal-font text-[#5865f2] block sm:hidden">
-                Login
-              </button>
-            </div>
+            <ButtonGroup />
           </div>
           <img
             src="/close.png"
@@ -246,74 +255,69 @@ export const Navigation = () => {
           />
         </div>
       </div>
-      <div
-        className={`${
-          NavStyles.sideNav
-        } h-screen w-full md:w-4/12  bg-white rounded-l-md absolute z-12 right-0 ${
-          isSubSlideOpen ? NavStyles.sideNavOpen : NavStyles.sideNavClose
-        } top-0`}
-      >
-        <div className="flex justify-between mx-6">
-          <div className="w-11/12 ">
-            <div
-              className="border-b py-6"
-              onClick={() => {
-                toggleSubSlideNav(false);
-                toggleSlideNav(true);
-              }}
-            >
-              <img src="/caret-l.png" className="inline mr-2 w-3 h-3" />
-              <h1 className="inline font-light text-sm py-3 my-2 rounded-md hover:underline">
-                Back
-              </h1>
+      {isSubSlideOpen && (
+        <div
+          className={`h-full w-full md:w-4/12  bg-white rounded-l-md absolute z-12 right-0 top-0`}
+        >
+          <div className="flex justify-between mx-6">
+            <div className="w-11/12 ">
+              <div
+                className="border-b py-6"
+                onClick={() => {
+                  toggleSubSlideNav(false);
+                  toggleSlideNav(true);
+                }}
+              >
+                <img src="/caret-l.png" className="inline mr-2 w-3 h-3" />
+                <h1 className="inline font-light text-sm py-3 my-2 rounded-md hover:underline">
+                  Back
+                </h1>
+              </div>
+              <ul
+                className={`py-4 ${NavStyles.ulMaxHeight} overflow-y-scroll scrollbar-hide`}
+              >
+                {(currentSubNav === "Safety" &&
+                  safetySubData.map((data, index) => (
+                    <div className="border-b py-3 px-2" key={index}>
+                      <h2 className="text-lg mb-2">{data.title}</h2>
+                      <ul className="px-3">
+                        {data.liText.map((li, index) => (
+                          <li
+                            className="font-light py-2 text-sm hover:underline"
+                            key={index}
+                          >
+                            <Link href={li.link}>{li.text}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))) ||
+                  moderatorAcademyData.map((data, index) => (
+                    <div className="border-b py-3 px-2" key={index}>
+                      <h2 className="text-lg mb-2">{data.title}</h2>
+                      <ul className="px-3">
+                        {data.liText.map((li, index) => (
+                          <li
+                            className="font-light py-2 text-sm hover:underline"
+                            key={index}
+                          >
+                            <Link href={li.link}>{li.text}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+              </ul>
+              <ButtonGroup />
             </div>
-            <ul
-              className={`py-4 ${NavStyles.ulMaxHeight} overflow-y-scroll scrollbar-hide`}
-            >
-              {(currentSubNav === "Safety" &&
-                safetySubData.map((data, index) => (
-                  <div className="border-b py-3 px-2" key={index}>
-                    <h2 className="text-lg mb-2">{data.title}</h2>
-                    <ul className="px-3">
-                      {data.liText.map((li, index) => (
-                        <li
-                          className="font-light py-2 text-sm hover:underline"
-                          key={index}
-                        >
-                          <Link href={li.link}>{li.text}</Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ))) ||
-                moderatorAcademyData.map((data, index) => (
-                  <div className="border-b py-3 px-2" key={index}>
-                    <h2 className="text-lg mb-2">{data.title}</h2>
-                    <ul className="px-3">
-                      {data.liText.map((li, index) => (
-                        <li
-                          className="font-light py-2 text-sm hover:underline"
-                          key={index}
-                        >
-                          <Link href={li.link}>{li.text}</Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ))}
-            </ul>
-            <button className="px-5 py-3 absolute bottom-6 z-10 text-white text-sm bg-[#5865f2] rounded-full flex items-center">
-              <img src="/download.png" className="inline w-4 h-4 mr-3" />
-              Download for Linux
-            </button>
+            <img
+              src="/close.png"
+              className="w-4 h-4 mt-7 cursor-pointer"
+              onClick={closeAll}
+            />
           </div>
-          <img
-            src="/close.png"
-            className="w-4 h-4 mt-7 cursor-pointer"
-            onClick={closeAll}
-          />
         </div>
-      </div>
+      )}
     </nav>
   );
 };
