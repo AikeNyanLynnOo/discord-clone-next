34efc657bff9d76c4923548d06585c9a3ac3b52d
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2023-02-05 16:01:08 +0630

    add right fonts or discord site, add redirect next config setting for downloads page

--- components/shared/PageHead.tsx
+++ components/shared/PageHead.tsx
@@ -11,12 +11,6 @@ export const PageHead = (props: { pageTitle: string }) => {
       <meta name="description" content="Created by a1k3" />
       <title>{props.pageTitle}</title>
       <link rel="icon" href="/images/discord-transparent.png" />
-      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-      <link
-        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&family=Poppins&family=Sigmar+One&display=swap"
-        rel="stylesheet"
-      ></link>
     </Head>
   );
 };
